882205404abec2b49c11a58404d4917ea198c7fa
Pichunov-N <npichunov@gmail.com>
2022-05-27 18:26:50 +0300

    finished single task

---
+++ script.js
@@ -0,0 +1,33 @@
+let myPromise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve("done"), 5000
+        // reject(new Error ("something going wrong")), 5000
+    });
+});
+
+myPromise
+    .then(
+        (result) => console.log(result),
+    )
+    .catch(
+        (error) => console.error(error)
+    )
+    .finally(
+        console.log("Promise was finished!")
+    )
+
+async function getMyPromise() {
+    try {
+        const promise = await myPromise;
+        return console.log(promise)
+    } catch (error) {
+        console.error(error);
+    } finally {
+        console.log("Finally finished")
+    }
+}
+getMyPromise()
+
+// console.log(myPromise)
+
+
